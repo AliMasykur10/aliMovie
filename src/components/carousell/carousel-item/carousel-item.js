7fe53a09012e3732518892f56bf18a62544d967f
AliMasykur10 <alimasykur2001@gmail.com>
2023-04-01 11:16:44 +0700

    menyelesaikan carousel

--- src/components/carousell/carousel-item/carousel-item.js
+++ src/components/carousell/carousel-item/carousel-item.js
@@ -1,7 +1,23 @@
+import "./carousel-item.css";
+import banner from "../../../assets/bannerFilm.png";
+
 const Carousel = () => {
   return (
     <>
-      <div class="carousel satu">satu</div>
+      <div className="carousel">
+        <div className="movie-banner">
+          <img src={banner} alt="" />
+        </div>
+        <div className="movie-desc">
+          <h1 className="title">Spiderman : The Last of Us</h1>
+          <div className="genre">
+            <p>Action</p>
+            <p>Drama</p>
+            <p>Romance</p>
+            <p>Comedy</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/components/carousell/carousel.js
+++ src/components/carousell/carousel.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
 import Carousel from "./carousel-item/carousel-item";
-import "./carousel.css";
 
 export default class SimpleSlider extends Component {
   render() {
     const settings = {
-      //   dots: true,
       infinite: true,
       speed: 500,
       slidesToShow: 1,
       slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      pauseOnHover: true,
     };
     return (
       <div>
